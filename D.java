b0359d685386e780c6841632a43ae32a374c2cde
ajay8602 <ajaymishrakte@gmail.com>
2021-04-14 12:45:16 +0530

    One More

---
+++ D.java
@@ -0,0 +1,12 @@
+public class D{
+    private String template;
+    private String defaultName = "Stranger";
+
+    public String getTemplate() {
+        return template;
+    }
+
+    public String getDefaultName() {
+        return defaultName;
+    }
+}
