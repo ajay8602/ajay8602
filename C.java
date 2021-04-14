6e1395103bde1bcf02fcc565a1defae3a2d328a7
ajay8602 <ajaymishrakte@gmail.com>
2021-04-14 12:36:05 +0530

    Add another file

---
+++ C.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class C{
+		public static void main(String[] args){
+			Scanner sc = new Scanner(System.in);
+			String str = sc.next();
+			System.out.println("Hello "+str);
+		}
+}
