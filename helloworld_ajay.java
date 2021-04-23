49cfcaf04bf65c33f01f80599598a19f00072109
ajay8602 <ajaymishrakte@gmail.com>
2021-04-23 23:11:17 +0530

    helloworld_ajay.java

---
+++ helloworld_ajay.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class helloworld_ajay{
+		public static void main(String[] args){
+		//	Scanner sc = new Scanner(System.in);
+		//	String str = sc.next();
+			System.out.println("Hello Mr. Ajay Your Dokcer File Created Successfully ");
+		}
+}
